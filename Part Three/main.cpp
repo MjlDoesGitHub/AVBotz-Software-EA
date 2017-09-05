6171112e51ed369679810323948eb58b81e12ca1
Matthew Lim <personal.matthewjlim@gmail.com>
2017-09-04 21:59:07 -0700

    Update main.cpp

--- Part Three/main.cpp
+++ Part Three/main.cpp
@@ -2,30 +2,29 @@
 #include <fstream>
 #include <string>
 #include <stdlib.h>
-#define INF 9999
 
 using namespace std;
 
-int minCoins(int values[],int y,int x) {
+int minimalCoins(int values[],int y,int x) {
     //Using array to output the minimum amount of coins that is required for the input
     int change[x+1];
     change[0] = 0;
     int i,z;
     for(z=1;z<=x;z++) {
-        int min = INF;
+        int minimal = 9999;
         for(i=0;i<y;i++) {
-            if(z>=values[i] && (change[z-values[i]]+1) < min)
-                min = change[z-values[i]]+1;
+            if(z>=values[i] && (change[z-values[i]]+1) < minimal)
+                minimal = change[z-values[i]]+1;
         }
-        // utput the least amount of coins to match our input using variable z
-        change[z] = min;
+        //Output the least amount of coins to match our input using variable "z"
+        change[z] = minimal;
     }
     return change[x];
 }
 
 int main()
 {
-    //Open Input File "coins2.in"
+    //Open Input File "coins.in"
     ifstream inFile;
     inFile.open("coins2.in");
 
@@ -55,16 +54,17 @@ int main()
     cout << "Input Coin Value #4: " << d << " coins" << endl;
 
     //Work with coins
-    int values[] = {a,b,c,d}; //Use a set of denominations (values of coins we can use)
+    int values[] = {a,b,c,d}; //Use a set of coin values we can use
     int n = sizeof(values)/sizeof(values[0]);
-    int min = minCoins(values,n,x);
-    cout<<"\nMin no. of coins required: " << min; cout <<endl;
+    int minimal = minimalCoins(values,n,x);
+    cout<<"\nMin no. of coins required: " << minimal; cout <<endl;
 
-    //Write final output to file "coins2.out"
+    //Write final output to file "coins.out"
     ofstream outFile;
     outFile.open("coins2.out");
-    outFile << min;
+    outFile << minimal;
     outFile.close();
 
     return 0;
+
 }
