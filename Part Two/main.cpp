fd148e34df15c24f9a3ada00ac2cac683e7391f0
Matthew Lim <personal.matthewjlim@gmail.com>
2017-09-16 20:25:58 -0700

    Update main.cpp

--- Part Two/main.cpp
+++ Part Two/main.cpp
@@ -1,4 +1,4 @@
-//Dear AVBotz, I couldn't find out how to work with decimals for this part. However, putting whole numbers will work just fine. Say you put 99 in the input file instead of 0.99, it will return the same output for both.
+//Dear AVBotz, I couldn't find out how to work with decimals for this assignment. However, putting whole numbers will work just fine. Say you put 99 in the input file instead of 0.99, it will return the same output for both.
 
 #include <iostream>
 #include <fstream>
