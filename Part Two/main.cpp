ac871acc469274381af44d3df828201b255cd4dc
Matthew Lim <personal.matthewjlim@gmail.com>
2017-09-16 23:57:52 -0700

    Update main.cpp

--- Part Two/main.cpp
+++ Part Two/main.cpp
@@ -1,5 +1,3 @@
-//Dear AVBotz, I couldn't find out how to work with decimals for this assignment. However, putting whole numbers will work just fine. Say you put 99 in the input file instead of 0.99, it will return the same output for both.
-
 #include <iostream>
 #include <fstream>
 #include <string>
