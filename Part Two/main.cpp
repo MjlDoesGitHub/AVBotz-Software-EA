918acac36f65a1304ab6c25252d2226d0536a048
Matthew Lim <personal.matthewjlim@gmail.com>
2017-09-25 23:23:20 -0700

    Update main.cpp

--- Part Two/main.cpp
+++ Part Two/main.cpp
@@ -8,15 +8,24 @@ using namespace std;
 ifstream inFile;
 ofstream outFile;
 
+//using intergers for this one... not precise
+
 int minimalCoins(int values[],int y,int x) {
-    //using array to output the minimum amount of coins that is required for the input
-    int change[x+1];
-    change[0] = 0;
-    int i,z;
-    for(z=1;z<=x;z++) {
-        int minimal = 9999;
+    //using array/table to output the minimum amount of coins that is required for the input
+
+    //change[z]is minimum number of coins needed for the variable z, therefore change[x] will have the output
+    int change[x+1]; //input plus 1
+    change[0] = 0; //if input is 0 then make chance equal to 0
+
+    int i,z; //using these variables for the upcoming function
+
+    //format the array/table to be infinite to make all inputs possible
+    for(z=1;z<=x;z++) { //calculate if 1 is less than or equal to the input value then repeat this function to find all required minimum coins from 1 to 9999 (infinite)
+        int minimal = 9999; //infinite values in the array/table for inputs
+
+        //locate the coins that are smaller than z
         for(i=0;i<y;i++) {
-            if(z>=values[i] && (change[z-values[i]]+1) < minimal)
+            if(z>=values[i] && (change[z-values[i]]+1) < minimal) //change[z] indicates the minimum amount of coins required
                 minimal = change[z-values[i]]+1;
         }
         //output the least amount of coins to match our input using variable z
@@ -44,10 +53,10 @@ int main()
     inFile >> x;
     cout << "Input Coin Value: " << x << " cents" << endl;
 
-    //work with coins
-    int values[] = {1,5,10,25}; //values of coins we can use)
-    int n = sizeof(values)/sizeof(values[0]);
-    int minimal = minimalCoins(values,n,x);
+    //calculate coin values
+    int values[] = {1,5,10,25}; //set of denominators or coins we can use
+    int y = sizeof(values)/sizeof(values[0]); //used to test the values
+    int minimal = minimalCoins(values,y,x);
     cout<<"\nMinimal usage of coins required: " << minimal; cout <<endl;
 
     //write final output to file "coins.out"
